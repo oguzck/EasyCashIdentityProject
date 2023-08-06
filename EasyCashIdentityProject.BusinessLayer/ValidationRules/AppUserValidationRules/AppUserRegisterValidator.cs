9a1f90e2d092001ce20b78d5886bcf0ee6bd2abf
oguzk <oguzcankilickaya@gmail.com>
2023-08-06 22:46:06 +0300

    FirstCommit

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x=>x.Name).NotEmpty().WithMessage("Ad Alanı Boş Geçilemez");
+            RuleFor(x=>x.Surname).NotEmpty().WithMessage("Soyad Alanı Boş Geçilemez");
+            RuleFor(x=>x.Username).NotEmpty().WithMessage("Kullanıcı Adı Alanı Boş Geçilemez");
+            RuleFor(x=>x.Email).NotEmpty().WithMessage("Email Alanı Boş Geçilemez");
+            RuleFor(x=>x.Password).NotEmpty().WithMessage("Şifre Alanı Boş Geçilemez");
+            RuleFor(x=>x.ConfirmPassword).NotEmpty().WithMessage("Şifre Tekrar Alanı Boş Geçilemez");
+            RuleFor(x=>x.Name).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter giriniz");
+            RuleFor(x=>x.Name).MinimumLength(2).WithMessage("Lütfen en az 2 karakter giriniz");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Parolalarınız eşleşmiyor");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli bir mail adresi giriniz");
+        }
+    }
+}
---
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230806151318_FkRelationShipsAdded.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyCashIdentityProject.DataAccessLayer.Migrations
+{
+    public partial class FkRelationShipsAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppUserID",
+                table: "CustomerAccount",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAccount_AppUserID",
+                table: "CustomerAccount",
+                column: "AppUserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerAccount_AspNetUsers_AppUserID",
+                table: "CustomerAccount",
+                column: "AppUserID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerAccount_AspNetUsers_AppUserID",
+                table: "CustomerAccount");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CustomerAccount_AppUserID",
+                table: "CustomerAccount");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserID",
+                table: "CustomerAccount");
+        }
+    }
+}
