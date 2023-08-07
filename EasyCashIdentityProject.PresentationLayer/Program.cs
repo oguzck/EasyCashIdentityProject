141c22fc0e0ed71ae44615f9caaec09437f7572f
oguzk <oguzcankilickaya@gmail.com>
2023-08-08 00:46:35 +0300

    mailsender added

--- EasyCashIdentityProject.PresentationLayer/Program.cs
+++ EasyCashIdentityProject.PresentationLayer/Program.cs
@@ -1,7 +1,17 @@
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>()
+    .AddErrorDescriber<CustomIdentityValidator>();
+
+
 
 var app = builder.Build();
 
@@ -17,6 +27,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
